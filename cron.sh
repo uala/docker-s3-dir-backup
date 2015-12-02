3b8c10e20016d4c04d6ffe8da8d6576caa91ab71
Mohammad Naghavi <mohamnag@gmail.com>
2015-12-02 16:16:13 +0100

    save and recover AWS_* variables too

--- cron.sh
+++ cron.sh
@@ -2,6 +2,7 @@
 
 # save env variables
 set | grep "BACKUP_" > /root/env
+set | grep "AWS_" >> /root/env 
 
 # change cron schedule
 sed -i "s,CRON_SCHEDULE*,${BACKUP_CRON_SCHEDULE},g" /etc/cron.d/backup-cron
