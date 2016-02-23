d4ae4522f852900e26adbd97582710d409f76cd3
Mohammad Naghavi <mohamnag@gmail.com>
2016-02-23 14:32:28 +0100

    fixing absolute backup path in archive

--- backup.sh
+++ backup.sh
@@ -2,13 +2,13 @@
 
 echo "creating archive..."
 
-eval "export BACKUP_DST_FULL_PATH=${BACKUP_TGT_DIR}${BACKUP_FILE_NAME}.tar.gz"
+BACKUP_DST_FULL_PATH=${BACKUP_TGT_DIR}${BACKUP_FILE_NAME}.tar.gz
 BACKUP_DST_DIR=$(dirname "${BACKUP_DST_FULL_PATH}")
 
-mkdir -p ${BACKUP_DST_DIR} 
+mkdir -p ${BACKUP_DST_DIR}
 echo "Gzipping ${BACKUP_SRC_DIR} into ${BACKUP_DST_FULL_PATH}" 
 
-tar -czf ${BACKUP_DST_FULL_PATH} ${BACKUP_SRC_DIR}
+tar -czf ${BACKUP_DST_FULL_PATH} -C ${BACKUP_SRC_DIR} .
 
 echo "archive created, uploading..."
 /usr/bin/aws s3 sync ${BACKUP_TGT_DIR} s3://${BACKUP_S3_BUCKET}
