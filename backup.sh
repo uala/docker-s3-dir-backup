89a7a02b10f408580adc77fac79e663e21957a50
Luca <leen15@msn.com>
2016-08-16 15:26:11 +0200

    Change logic for compare the last backup with the new one

--- backup.sh
+++ backup.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+COMPARE_DIR="/compare_dir/"
+
+echo "backup started at $(date +%Y-%m-%d_%H:%M:%S)"
+
 echo "creating archive..."
 
 # this has to be executed like this, because we have two level expansion in variables
 eval "export BACKUP_DST_FULL_PATH=${BACKUP_TGT_DIR}${BACKUP_FILE_NAME}.tar.gz"
+eval "export COMPARE_DST_FULL_PATH=${COMPARE_DIR}${BACKUP_FILE_NAME}.tar.gz"
 
 BACKUP_DST_DIR=$(dirname "${BACKUP_DST_FULL_PATH}")
 
-mkdir -p ${BACKUP_DST_DIR}
-echo "Gzipping ${BACKUP_SRC_DIR} into ${BACKUP_DST_FULL_PATH}" 
+echo "Gzipping ${BACKUP_SRC_DIR} into ${COMPARE_DST_FULL_PATH}" 
+tar -czf ${COMPARE_DST_FULL_PATH} -C ${BACKUP_SRC_DIR} .
 
-tar -czf ${BACKUP_DST_FULL_PATH} -C ${BACKUP_SRC_DIR} .
+if cmp -s -i 8 "$BACKUP_DST_FULL_PATH" "$COMPARE_DST_FULL_PATH"
+then
+   echo "Archive is the same of the old one, do nothing."
+else
+   echo "Archive is different from the old one, uploading to s3..."
+   mkdir -p ${BACKUP_DST_DIR}
+   mv "$COMPARE_DST_FULL_PATH" "$BACKUP_DST_FULL_PATH"
+   #echo "archive created, uploading..."
+   /usr/bin/aws s3 sync ${BACKUP_TGT_DIR} s3://${BACKUP_S3_BUCKET}
+fi
 
-echo "archive created, uploading..."
-/usr/bin/aws s3 sync ${BACKUP_TGT_DIR} s3://${BACKUP_S3_BUCKET}
 
-echo "backup finished"
+echo "backup finished at $(date +%Y-%m-%d_%H:%M:%S)"
